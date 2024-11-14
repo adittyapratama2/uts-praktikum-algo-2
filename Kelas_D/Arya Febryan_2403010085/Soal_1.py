4dd62028d55074ddcaec96fc77f6c1de9d3259aa
AryaFn06 <2403010085@unper.ac.id>
2024-11-14 17:41:07 +0700

    Add files via upload

---
+++ Kelas_D/Arya Febryan_2403010085/Soal_1.py
@@ -0,0 +1,15 @@
+# Arya Febryan
+# 2403010085
+# Program untuk menghitung luas lingkaran
+
+# Meminta input jari-jari dari pengguna
+jari_jari = float(input("Masukkan jari-jari lingkaran: "))
+
+# Nilai pi
+pi = 3.14
+
+# Menghitung luas lingkaran
+luas_lingkaran = pi * jari_jari**2
+
+# Menampilkan hasil
+print(f"Luas lingkaran dengan jari-jari {jari_jari} adalah {luas_lingkaran}")
