8c3091d3b7e7ac2ee04252279eff1456af42b358
Ghaysan <2403010145@unper.ac.id>
2024-11-14 18:14:18 +0700

    Add files via upload

---
+++ Kelas_F/MuhammadGhaysanNaufalAziz_2403010145/soal_4.py
@@ -0,0 +1,14 @@
+def hitung_konsonan(teks):
+    konsonan = "Welcome to the Hisoka Zonee"  
+    jumlah_konsonan = 0
+    
+    for char in teks:
+        if char in konsonan:
+            jumlah_konsonan += 1
+    
+    return jumlah_konsonan
+
+teks_input = input("Masukkan sebuah kalimat atau kata: ")
+
+jumlah = hitung_konsonan(teks_input)
+print(f"Jumlah huruf konsonan dalam string adalah: {jumlah}")
