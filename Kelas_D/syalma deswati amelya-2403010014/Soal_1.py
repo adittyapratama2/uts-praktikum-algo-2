b771b3370044cdbb03bb7180906fdd5721cff28d
syalmamelya <syalmalutfy@gmail.com>
2024-11-14 20:17:58 +0700

    Add files via upload

---
+++ Kelas_D/syalma deswati amelya-2403010014/Soal_1.py
@@ -0,0 +1,8 @@
+
+print("-----menghitung luas lingkaran-----")
+print(" ")
+phi = 3.14
+print("Masukan r")
+r = float(input())
+v = phi * r * r 
+print(f"Hasil adalah:{v}")
---
+++ Kelas_D/syalma deswati amelya-2403010014/Soal_4.py
@@ -0,0 +1,11 @@
+print("-----Menghitung huruf konsonan-----")
+print(" ")
+kalimat = input('masukan kalimat: ')
+string = kalimat.lower()
+count = 0
+list1 = ["a","i","u","e","o"]
+for char in string:
+    if char in list1:
+        count = count+1
+
+print("jumlah huruf vokal adalah:",count)
