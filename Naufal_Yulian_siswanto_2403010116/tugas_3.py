c5e6ef4273c0571fb893200c5f68f91186d5966f
NaufalYulianSiswanto <yuliannaufal5@gmail.com>
2024-11-14 23:01:32 +0700

    Add files via upload

---
+++ Naufal_Yulian_siswanto_2403010116/tugas_3.py
@@ -0,0 +1,10 @@
+# Meminta pengguna memasukan tiga kata
+kata1=input("masukan kata pertama")
+kata2=input("masukan kata kedua")
+kata3=input("masukan kata ketiga")
+
+# Gabungkan ketiga kata dengan sepasi di antara setiap kata
+kalimat = kata1 + " "+ kata2 +" "+ kata3
+
+# menampilkan kalimat hasil gabungan
+print("kalimat gabungan:, kalimat")
