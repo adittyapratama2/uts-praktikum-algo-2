0b3708a7f1b9ea02b79cdf0e20b862260911bfb1
Rayyan0805 <2403010135@unper.ac.id>
2024-11-14 18:23:45 +0700

    Delete Soal_1.py

--- Soal_1.py
+++
@@ -1,6 +0,0 @@
-#MENGHITUNG LUAS LINGKARAN
-phi = 3.14
-jari_jari = float(input("masukan jari-jarinya yang mau dimasukin= "))
-
-luas = phi*jari_jari*2
-print(f"Jadi Luas Lingkaran tersebut adalah = {luas}")
