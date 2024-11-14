8d64abb538efbfa780dc09bfed138f8f8a9abd64
Hanifirsd <hanifirsyaddudin@gmail.com>
2024-11-14 18:06:31 +0700

    Add files via upload

---
+++ Kelas_G/Soal_1.py
@@ -0,0 +1,11 @@
+# Mendefinisikan nilai phi
+phi = 3.14
+
+# Input jari-jari
+jari_jari = float(input("Masukkan jari-jari: "))
+
+# Menghitung luas lingkaran
+luas_lingkaran = phi*jari_jari**2
+
+# Menampilkan hasil
+print(f"Luas lingkaran: {luas_lingkaran}")
