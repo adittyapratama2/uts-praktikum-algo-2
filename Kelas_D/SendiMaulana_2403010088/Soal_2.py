f1fe33c856abb826448af3bc27d2fdc211d88fa9
Sendi-maulana <2403010088@unper.ac.id>
2024-11-14 13:50:04 +0700

    Add files via upload

---
+++ Kelas_D/SendiMaulana_2403010088/Soal_2.py
@@ -0,0 +1,17 @@
+#Nama:sendi maulana
+#NIM:2403010088
+#Kelas:TI-D-24
+print("-----Konversi detik,menit,jam-----")
+print(" ")
+def konversi_detik( detik ):
+    jam = detik // 3600  
+    menit = (detik % 3600) // 60  
+    detik_sisa = detik % 60 
+    return jam, menit, detik_sisa
+
+detik_input = int(input("Masukkan jumlah detik: "))
+
+jam, menit, detik_sisa = konversi_detik(detik_input)
+
+print(f"{detik_input} detik = {jam} jam, {menit} menit, {detik_sisa} detik")
+
