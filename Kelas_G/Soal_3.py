f9983652daa224432958b9fcf8d3bc7f7bc26553
tsaniilham <2403010167@unper.ac.id>
2024-11-14 17:23:51 +0700

    Add files via upload

---
+++ Kelas_G/Soal_3.py
@@ -0,0 +1,10 @@
+# Meminta input tiga kata dari pengguna
+kata1 = input("Masukkan kata pertama: ")
+kata2 = input("Masukkan kata kedua: ")
+kata3 = input("Masukkan kata ketiga: ")
+
+# Menggabungkan kata-kata tersebut menjadi satu kalimat
+kalimat = kata1 + " " + kata2 + " " + kata3
+
+# Menampilkan kalimat hasil penggabungan
+print("Kalimat yang terbentuk: ", kalimat)
---
+++ Kelas_G/Soal_4.py
@@ -0,0 +1,7 @@
+def hitung_konsonan(kalimat):
+  vokal = 'aiueo'
+  return len(kalimat) - sum(kalimat.lower().count(v) for v in vokal)
+
+kalimat = "Ini adalah contoh kalimat"
+hasil = hitung_konsonan(kalimat)
+print("Jumlah konsonan:", hasil)
