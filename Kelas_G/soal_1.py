009bd71aefb380541d2219e8408bb742886d313b
Akmalfthn16 <2403010180@unper.ac.id>
2024-11-14 16:36:59 +0700

    Add files via upload

---
+++ Kelas_G/soal_1.py
@@ -0,0 +1,6 @@
+print ("Menghitung luas lingkaran")
+# Mendeklarasikan nilai pi
+pi = 3.14
+jari_jari = float(input("Masukan jari-jari lingkaran: "))
+luas_lingkaran = pi * jari_jari ** 2
+print(F"nilai luas lingkaran = {luas_lingkaran}")
