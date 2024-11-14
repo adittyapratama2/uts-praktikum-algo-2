8eed02e228cb1dcb027bc18f21b98f552db6a252
FadilNrhll0021 <2403010173@unper.ac.id>
2024-11-14 18:08:46 +0700

    Add files via upload

---
+++ Kelas_G/Soal_2.py
@@ -0,0 +1,10 @@
+def konversi_waktu(total_detik):
+    jam = total_detik // 3600
+    sisa_detik = total_detik % 3600
+    menit = sisa_detik // 60
+    detik = sisa_detik % 601
+    return jam, menit, detik
+
+total_detik = int(input("Masukkan jumlah detik:"))
+jam, menit, detik = konversi_waktu(total_detik)
+print(f"{total_detik} detik = {jam} jam, {menit} menit, {detik} detik")
