1652707ee686f100b6c41bf2b5e10e6f0e7128ad
MuhammadChaedar <247006111167@student.unsil.ac.id>
2024-11-14 20:33:55 +0700

    Jawaban UTS ALPRO

---
+++ Kelas_E/anggamaulanaridwan_2403010123/No 1.py
@@ -0,0 +1,11 @@
+#Meminta input jari-jari dari pengguna
+jari_jari = float(input("Masukkan untuk jari-jari: "))
+
+#Nilai π (pi)
+pi = 3.14
+
+#Menghitung luas lingkaran
+luas_lingkaran = pi * jari_jari**2
+
+#Menampilkan hasil dari luas lingkaran
+print(f"Hasil Luas lingkaran: {luas_lingkaran:.2f}")
---
+++ Kelas_E/anggamaulanaridwan_2403010123/No 3.py
@@ -0,0 +1,10 @@
+#Meminta input kata pertama, kedua, dan ketiga
+kata_pertama = input("Masukkan kata pertama anda: ")
+kata_kedua = input("Masukkan kata kedua anda: ")
+kata_ketiga = input("Masukkan kata ketiga anda: ")
+
+#Menggabungkan ketiga kata menjadi satu kalimat dengan spasi dari program
+kalimat_gabungan = kata_pertama + " " + kata_kedua + " " + kata_ketiga
+
+#Menampilkan hasil penggabungannya
+print("Hasil dari penggabungannya adalah:", kalimat_gabungan)
