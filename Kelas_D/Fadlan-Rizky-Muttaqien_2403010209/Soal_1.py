bf44c417ca09d20891b83d694bd3dc99c50ed290
FadlanRizky <frm140305@gmail.com>
2024-11-14 18:02:25 +0700

    Add files via upload

---
+++ Kelas_D/Fadlan-Rizky-Muttaqien_2403010209/Soal_1.py
@@ -0,0 +1,13 @@
+# Zianarah Zihan
+# 2403010101
+# Meminta input jari-jari dari pengguna
+jari_jari = float(input("Masukkan jari-jari: "))
+
+# Menggunakan nilai π = 3.14
+pi = 3.14
+
+# Menghitung luas lingkaran
+luas = pi * (jari_jari ** 2)
+
+# Menampilkan hasil
+print("Luas lingkaran:", round(luas, 2))
