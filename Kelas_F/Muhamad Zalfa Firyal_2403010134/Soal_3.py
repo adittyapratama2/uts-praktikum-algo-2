057e2c723ae60dc4503e227766cd298651f7f6d0
MuhamadZalfa <2403010134@unper.ac.id>
2024-11-14 22:10:39 +0700

    Add files via upload

---
+++ Kelas_F/Muhamad Zalfa Firyal_2403010134/Soal_3.py
@@ -0,0 +1,10 @@
+#Nama:Muhamad Zalfa Firyal
+#NIM:2403010134
+#Kelas:TI-F-24
+print("-----Menggabungkan Tiga Kata-----")
+print(" ")
+a = input("Masukkan kata pertama: ")
+b = input("Masukkan kata kedua: ")
+c = input("Masukkan kata ketiga: ")
+d = a + " " + b + " " + c
+print(d)
