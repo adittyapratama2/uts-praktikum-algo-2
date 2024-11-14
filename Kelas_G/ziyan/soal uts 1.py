d44023fe10d8ac679a723f619c7da66c2778252a
yangzftrh314 <2403010175@unper.ac.id>
2024-11-14 18:29:01 +0700

    Delete Kelas_G/ziyan directory

--- Kelas_G/ziyan/soal uts 1.py
+++
@@ -1,7 +0,0 @@
-# menghitung luas lingkaran
-
-phi = 3.14
-jari_jari = float(input("masukkan nilai jari-jari : "))
-luas_lingkaran = phi*jari_jari**2
-
-print (f"nilai luas lingkaran adalah : {luas_lingkaran}")
--- Kelas_G/ziyan/soal uts 5.py
+++
@@ -1,6 +0,0 @@
-kata = input("Masukan kata :")
-posisi_awal = int(input("Masukan posisi awal : "))
-panjang_karakter = int(input("Masukan panjang karakter :"))
-
-substring = kata[posisi_awal:posisi_awal+panjang_karakter]
-print("Substring:",substring)
