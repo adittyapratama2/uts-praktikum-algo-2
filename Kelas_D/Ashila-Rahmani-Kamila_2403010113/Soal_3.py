fd5d048fef1a90b403f92b6bbd413c5588f23ac1
AshilaRahmaniKamila06 <ashilarahmani598@gmail.com>
2024-11-14 20:58:03 +0700

    Add files via upload

---
+++ Kelas_D/Ashila-Rahmani-Kamila_2403010113/Soal_3.py
@@ -0,0 +1,8 @@
+
+print("-----Menggabungkan Tiga Kata-----")
+print(" ")
+a = input("Masukkan kata pertama: ")
+b = input("Masukkan kata kedua: ")
+c = input("Masukkan kata ketiga: ")
+d = a + " " + b + " " + c
+print(d)
