ccfaf7526e379b6bf9928cedc2286d9a2a57e877
Atunk Oi <110760968+atunkoi@users.noreply.github.com>
2024-11-14 21:30:36 +0700

    Menyelesaikan semua soal UTS

---
+++ Kelas_D/AgungIlham_2403010090/Soal_1.py
@@ -0,0 +1,12 @@
+'''
+Agung Ilham Setiyadi
+2403010090
+Menghitung Luas Lingkaran
+'''
+
+jari_jari = float(input("masukan jari-jari: "))
+
+luas = 3.14 * jari_jari**2
+
+print("Masukan jari-jari: ",jari_jari)
+print("Luas lingkaran: ",luas)
