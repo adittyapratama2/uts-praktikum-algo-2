997a00d3aa28e8d5d3844fc176c4913880de745d
Amalawati <2403010131@unper.ac.id>
2024-11-14 21:35:02 +0700

    Delete Kelas_F/soal_3.py

--- Kelas_F/soal_3.py
+++
@@ -1,6 +0,0 @@
-kata_pertama = input("masukan kata pertama : ")
-kata_kedua = input("masukan kata kedua : ")
-kata_ketiga = input("masukan kata ketiga : ")
-
-kata = kata_pertama +  ' ' + kata_kedua +  ' ' + kata_ketiga
-print(kata)
