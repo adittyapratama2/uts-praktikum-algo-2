ebc418223787cdf5a56fe73df9e18bb8454b483a
HirsanMaulana <hirsanmaulana142@gmail.com>
2024-11-14 17:58:19 +0700

    Add files via upload

---
+++ Kelas_G/Soal_3.py
@@ -0,0 +1,10 @@
+# Meminta input dari pengguna
+kata1 = input("Masukkan kata pertama: ")
+kata2 = input("Masukkan kata kedua: ")
+kata3 = input("Masukkan kata ketiga: ")
+
+# Menggabungkan ketiga kata dengan spasi di antara setiap kata
+kalimat = f"{kata1} {kata2} {kata3}"
+
+# Menampilkan hasil
+print("Kalimat yang digabungkan:", kalimat)
