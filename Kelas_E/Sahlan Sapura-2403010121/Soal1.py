f3ff91661a101e068f0baeea7d1bb5848de6e8ef
pekyu02 <2403010025@unper.ac.id>
2024-11-14 21:21:01 +0700

    Uts Praktikum alpro 2 Sahlan Saputra P

---
+++ Kelas_E/Sahlan Sapura-2403010121/Soal1.py
@@ -0,0 +1,6 @@
+jari = float(input("Jari-jari: "))
+
+luas = 3.14 * (jari**2)
+
+print("Jadi luas nya adalah: " , luas)
+
---
+++ Kelas_E/Sahlan Sapura-2403010121/Soal3.py
@@ -0,0 +1,8 @@
+#Sahlan Saputra P
+#2403010121
+#Menggabungkan tiga kata, menjadikan satu kalimat
+
+kalimat_1 = input("masukan kata kesatu: ")
+kalimat_2 = input("masukan kata kedua: ")
+kalimat_3 = input("masukan kata ketiga: ")
+print(kalimat_1 + " " + kalimat_2 + " " + kalimat_3)
