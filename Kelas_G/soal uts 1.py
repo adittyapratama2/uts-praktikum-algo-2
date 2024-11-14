ca7d93f62e2271bd5c68e940cb4fecf5e28e1961
Insandzulfiqor021204 <2403010174@unper.ac.id>
2024-11-14 17:44:58 +0700

    PR InsanDzulfiqor_2403010174

---
+++ Kelas_G/soal uts 1.py
@@ -0,0 +1,10 @@
+#NAMA : INSAN DZULFIQOR
+#NIM : 2403010174
+#KELAS : G
+# menghitung luas lingkaran
+
+phi = 3.14
+jari_jari = float(input("masukkan nilai jari-jari : "))
+luas_lingkaran = phi*jari_jari**2
+
+print (f"nilai luas lingkaran adalah : {luas_lingkaran}")
