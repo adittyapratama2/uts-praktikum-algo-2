e0677b857ebf6d529412571916fb22a04e29aeef
isanuchiha144 <2403010118@unper.ac.id>
2024-11-14 21:35:23 +0700

    Menambahkan Jawaban UTS

---
+++ Kelas_E/Ihsan-Abdan_2403010118/soal_3.py
@@ -0,0 +1,8 @@
+#proses menggabungkan ketiga kata
+kata_pertama = input ( ' masukan kata pertama:')
+kata_kedua = input ( ' masukan kata kedua: ')
+kata_ketiga = input ( 'masukan kata ketiga:')
+# hasil penggabungan kata pertama kata kedua kata ketiga
+hasil_penggabungan = kata_pertama + kata_kedua + kata_ketiga
+#hasil
+print('hasil_penggabungan: ' + hasil_penggabungan)
