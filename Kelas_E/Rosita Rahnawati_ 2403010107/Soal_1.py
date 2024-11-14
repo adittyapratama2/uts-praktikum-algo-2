b3b2e64752c76843c2006fcc386f5df722d74d1b
RositaRahnawati <2403010107@unper.ac.id>
2024-11-14 20:21:44 +0700

    Add files via upload

---
+++ Kelas_E/Rosita Rahnawati_ 2403010107/Soal_1.py
@@ -0,0 +1,8 @@
+#deklarasikan nilai pi
+pi = 3.14
+#masukan nilai jari-jari
+jari_jari = float  ((input("masukan jari_jari lingkaran:")))
+#menghitung luas lingkaran 
+luas= pi* jari_jari**2
+#tampilkan nilai luas
+print (luas)
---
+++ Kelas_E/Rosita Rahnawati_ 2403010107/soal_2.py
@@ -0,0 +1,7 @@
+
+total_detik = int(input("Masukkan waktu dalam bentuk detik: "))
+jam = total_detik // 3600
+sisa_detik = total_detik % 3600
+menit = sisa_detik // 60
+detik = sisa_detik % 60 
+print(f"Hasil konversi: {jam} jam, {menit} menit, {detik} detik")
