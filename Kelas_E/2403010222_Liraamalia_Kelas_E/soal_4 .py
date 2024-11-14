fdde2df5464485277d462252d2587681e9035454
Liraamla12 <liraamalia234@gmail.com>
2024-11-14 21:40:27 +0700

    selesai pak

---
+++ Kelas_E/2403010222_Liraamalia_Kelas_E/soal_4 .py
@@ -0,0 +1,8 @@
+#Mengambil karakter dari kata
+kalimat = str(input("Masukkan kalimat: "))
+jumlah = 0
+vowel = "aiueoAIUEO"
+for x in kalimat:
+    if x.isalpha() and x not in vowel:
+        jumlah += 1
+print("Jumlah Huruf Konsonan:", jumlah)
---
+++ Kelas_E/2403010222_Liraamalia_Kelas_E/soal_5 .py
@@ -0,0 +1,5 @@
+kata = input("Masukkan sebuah kata")
+posisi_awal = int(input("masukkan posisi awal:"))
+panjang_karakter = int(input("masukkan panjang karakter yang ingin di ambil:"))
+substring = kata[posisi_awal+panjang_karakter]
+print (f"substring yang diambil dari posisi {posisi_awal} dengan panjang {panjang_karakter} adalah: {substring}")
