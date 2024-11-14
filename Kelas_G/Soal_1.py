3b3cb7ac8b326ae4cba0152dd3b33b375d7dc324
Agung pramulya <Agungpramulya15@gmail.com>
2024-11-14 17:11:28 +0700

    Add files via upload

---
+++ Kelas_G/Soal_1.py
@@ -0,0 +1,10 @@
+import math
+
+# Jari-jari lingkaran
+radius = 3.14
+
+# Menghitung luas lingkaran
+luas_lingkaran = math.pi * radius**2
+
+# Menampilkan hasil
+print("Luas lingkaran dengan radius", radius, "adalah:", luas_lingkaran)
