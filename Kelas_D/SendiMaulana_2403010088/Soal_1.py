e5ae0bffe4aab5230764890e6b1687cf7b673733
Sendi-maulana <2403010088@unper.ac.id>
2024-11-14 16:21:35 +0700

    Add files via upload

---
+++ Kelas_D/SendiMaulana_2403010088/Soal_1.py
@@ -0,0 +1,10 @@
+#Nama:sendi maulana
+#NIM:2403010088
+#Kelas:TI-D-24
+print("-----menghitung luas lingkaran-----")
+print(" ")
+phi = 3.14
+print("Masukan r")
+r = float(input())
+v = phi * r * r 
+print(f"Hasil adalah:{v}")
