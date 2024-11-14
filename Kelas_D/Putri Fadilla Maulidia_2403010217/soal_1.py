b8585e534c9c8b62cee400243970cfa9b286cef4
scaeraputri <putrifadila080406@gmail.com>
2024-11-14 20:20:41 +0700

    Create soal_1.py

---
+++ Kelas_D/Putri Fadilla Maulidia_2403010217/soal_1.py
@@ -0,0 +1,12 @@
+#Putri Fadilla Maulidia_2403010217
+#Menghitung Luas Lingkaran
+
+#input
+pi=3.14
+jarijari=float(input("Masukkan jari-jari lingkaran:"))
+
+#rumus luas lingkaran
+luas=pi*jarijari**2
+
+#output
+print("Luas lingkaran:",luas)
