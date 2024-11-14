891f5ab612364d4f9e1950dbafbde5c3db14c9e1
DarizeR <daris9897@gmail.com>
2024-11-14 18:50:52 +0700

    Add files via upload

---
+++ Kelas_G/Soal_1.py
@@ -0,0 +1,12 @@
+#NAMA : MUHAMMAD DARIS RIF'AN RAMADHAN
+#NIM  : 2403010164
+import math
+
+# Mendefinisikan jari-jari lingkaran
+jari_jari = 3.14  # Misalnya jari-jari lingkaran adalah 3.14
+
+# Menghitung luas lingkaran
+luas = math.pi * (jari_jari ** 2)
+
+# Menampilkan hasil
+print("Luas lingkaran dengan jari-jari", jari_jari, "adalah:", luas)
---
+++ Kelas_G/Soal_3.py
@@ -0,0 +1,12 @@
+#NAMA : MUHAMMAD DARIS RIF'AN RAMADHAN
+#NIM  : 2403010164
+# Meminta input tiga kata dari pengguna
+kata1 = input("Masukkan kata pertama: ")
+kata2 = input("Masukkan kata kedua: ")
+kata3 = input("Masukkan kata ketiga: ")
+
+# Menggabungkan kata-kata tersebut menjadi satu kalimat
+kalimat = kata1 + " " + kata2 + " " + kata3
+
+# Menampilkan kalimat hasil penggabungan
+print("Kalimat yang terbentuk: ", kalimat)
