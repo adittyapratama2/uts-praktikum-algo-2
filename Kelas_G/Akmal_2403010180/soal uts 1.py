bca919b7d4f3f385b9eb81afddb076aa2e7f0c44
Akmalfthn16 <2403010180@unper.ac.id>
2024-11-15 12:44:19 +0700

    Add files via upload

---
+++ Kelas_G/Akmal_2403010180/soal uts 1.py
@@ -0,0 +1,9 @@
+# Akmal Fathin Firmansyah
+# 2403010180
+# menghitung luas lingkaran
+
+phi = 3.14
+jari_jari = float(input("masukkan nilai jari-jari : "))
+luas_lingkaran = phi*jari_jari**2
+
+print (f"nilai luas lingkaran adalah : {luas_lingkaran}")
---
+++ Kelas_G/Akmal_2403010180/soal uts 3.py
@@ -0,0 +1,9 @@
+# Akmal Fathin Firmansyah
+# 2403010180
+kata_pertama = input("Masukan kata pertama : ")
+kata_kedua = input("Masukan kata kedua : ")
+kata_ketiga = input("Masukan kata ketiga :")
+
+kalimat_gabungan = kata_pertama +" "+ kata_kedua +" "+ kata_ketiga
+
+print("Hasil penggabungan : ", kalimat_gabungan)
