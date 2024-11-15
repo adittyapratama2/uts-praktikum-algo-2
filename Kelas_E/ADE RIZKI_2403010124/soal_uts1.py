ac3283fa4be8135cbd08dc2ddb0af9ae2356dbb0
Aderizki54 <2403010124@unper.ac.id>
2024-11-15 15:20:15 +0700

    UTS ALPRO

---
+++ Kelas_E/ADE RIZKI_2403010124/soal_uts1.py
@@ -0,0 +1,11 @@
+# Meminta pengguna untuk memasukkan jari-jari lingkaran
+jari_jari = float(input("Masukkan jari-jari: "))
+
+# Menentukan nilai π (pi)
+pi = 3.14
+
+# Menghitung luas lingkaran
+luas = pi * (jari_jari ** 2)
+
+# Menampilkan hasil
+print(f"Luas lingkaran: {luas:.2f}")
