a4144bab1049d0024f2bac90e9ce4dfbf8624f62
ImanuelTanuSenjaya <2403010143@unper.ac.id>
2024-11-15 08:15:38 +0700

    Add files via upload

---
+++ Kelas_F/ImanuelTanuSenjaya_24030143/soal_3.py
@@ -0,0 +1,13 @@
+#imanuel tanu senjaya 2403010143
+# Program untuk menggabungkan tiga kata
+
+# Meminta input kata pertama, kedua, dan ketiga
+kata_pertama = input("Masukkan kata pertama: ")
+kata_kedua = input("Masukkan kata kedua: ")
+kata_ketiga = input("Masukkan kata ketiga: ")
+
+# Menggabungkan ketiga kata dengan spasi
+kalimat_gabungan = kata_pertama + " " + kata_kedua + " " + kata_ketiga
+
+# Menampilkan hasil penggabungan
+print(f"Hasil penggabungan: {kalimat_gabungan}")
