ecb76942d9dc74eaa49a97afc1c864a53a2fe4ba
Ripki14 <2403010168@unper.ac.id>
2024-11-15 07:53:48 +0700

    PR Muhammad Ripki Nur Hidayat_2403010168

---
+++ Kelas_G/soal uts no 1.py
@@ -0,0 +1,13 @@
+# Program untuk menghitung luas lingkaran
+
+# Menggunakan nilai π (pi)
+phi = 3.14
+
+# Meminta pengguna untuk memasukkan jari-jari lingkaran
+jari_jari = float (input(" 7 "))
+
+# Menghitung luas lingkaran
+luas = phi *(jari_jari**2)
+
+# Menampilkan hasil
+print("luas lingkaran = ", luas)
