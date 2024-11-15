0042a3bf41f5ea05c460a971d4820f458f30fbd1
andiabdillah101 <2403010117@unper.ac.id>
2024-11-15 16:12:35 +0700

    Jawaban UTS praktek alpro

---
+++ Kelas_E/Gilang Ramadhan_2403010117/soal_3.py
@@ -0,0 +1,10 @@
+# Meminta pengguna untuk memasukkan tiga kata
+kata_pertama = input("Masukkan kata pertama: ")
+kata_kedua = input("Masukkan kata kedua: ")
+kata_ketiga = input("Masukkan kata ketiga: ")
+
+# Menggabungkan ketiga kata dengan spasi
+kalimat = kata_pertama + " " + kata_kedua + " " + kata_ketiga
+
+# Menampilkan hasil penggabungan
+print("Hasil penggabungan:", kalimat)
