a0a3544a8ff58cd439cc556c15d66f8c3c466f3f
JerryTopG <2203010512@unper.ac.id>
2024-11-23 09:47:34 +0700

    Update Soal_1.py

--- Kelas_G/JerryHilmanFirmansyah_2203010512/Soal_1.py
+++ Kelas_G/JerryHilmanFirmansyah_2203010512/Soal_1.py
@@ -6,5 +6,5 @@ print(f"Luas lingkaran: {luas}")
 
 #Jerry Hilman FIrmansyah 2203010512
 #program diatas merupakan program untuk menghitung luas lingkaran
-#menggunakan 1 inputan yaitu jari-jari lingkarang
+#menggunakan 1 inputan yaitu jari-jari lingkaran
 #menggunakan rumus pi dikali jari-jari panggat 2
